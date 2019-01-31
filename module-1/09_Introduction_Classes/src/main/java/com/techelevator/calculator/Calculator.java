93d1c390cf3da1dee312826d31046949e3a26f6e
Andrew Frank <andrew@techelevator.com>
2019-01-31 15:26:14 -0500

    Add solutions for Introduction to Classes

---
+++ module-1/09_Introduction_Classes/src/main/java/com/techelevator/calculator/Calculator.java
@@ -0,0 +1,35 @@
+package com.techelevator.calculator;
+
+public class Calculator {
+	
+	private int currentValue = 0;
+	
+	public int getCurrentValue() {
+		return currentValue;
+	}
+	
+	public int add(int addend) {
+		currentValue += addend;
+		return currentValue;
+	}
+
+	public int subtract(int subtrahend) {
+		currentValue -= subtrahend;
+		return currentValue;
+	}
+
+	public int multiply(int multiplier) {
+		currentValue *= multiplier;
+		return currentValue;
+	}
+
+	public int power(int exponent) {
+		currentValue = (int)Math.pow(currentValue, (double)exponent);
+		return currentValue;
+	}
+
+	public void reset() {
+		currentValue = 0;
+	}
+
+}
